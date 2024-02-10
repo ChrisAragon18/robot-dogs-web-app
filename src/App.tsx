7c1451964034f402a2df639707e5d8b7e77b06ca
Christopher Aragon <christopher.aragon2018@gmail.com>
2024-02-10 04:24:39 -0500

    Deleted App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Hero from './pages/Hero';
-import Home from './pages/Home';
-import About from './pages/About';
-import Analysis from './pages/Analysis';
-import Control from './pages/Control';
-import Surveillance from './pages/Surveillance';
-import ROSLIB from 'roslib'; // Import ROSLIB
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Hero from './pages/Hero'
+import Home from './pages/Home'
+import About from './pages/About'
+import Analysis from './pages/Analysis'
+import Control from './pages/Control'
+import Surveillance from './pages/Surveillance'
+import ROSLIB from 'roslib' // Import ROSLIB
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
@@ -14,26 +14,26 @@ function App() {
   // Function to check the connection status with the robot
   const checkConnection = async () => {
     try {
-      const ros = new ROSLIB.Ros({ url: 'ws://robot-ip-address:port' });
+      const ros = new ROSLIB.Ros({ url: 'ws://robot-ip-address:port' })
 
       await new Promise((resolve, reject) => {
-        ros.on('connection', resolve);
-        ros.on('error', reject);
+        ros.on('connection', resolve)
+        ros.on('error', reject)
       })
 
-      setIsConnected(true);
+      setIsConnected(true)
     } catch (error) {
-      setIsConnected(false);
-      console.error('Error connecting to ROS bridge:', error);
+      setIsConnected(false)
+      console.error('Error connecting to ROS bridge:', error)
     }
   }
 
   // Check the connection status at regular intervals
   useEffect(() => {
-    const intervalId = setInterval(checkConnection, 5000); // Check every 5 seconds
+    const intervalId = setInterval(checkConnection, 5000) // Check every 5 seconds
 
     // Clean up the interval on unmount
-    return () => clearInterval(intervalId);
+    return () => clearInterval(intervalId)
   }, [])
 
   return (
@@ -48,7 +48,7 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
 export default App
