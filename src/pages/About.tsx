64bdffada459b2194f06adf16068446baf5d8790
Francisco <paquito1214.2017@gmail.com>
2024-02-20 19:37:29 -0500

    About us page update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,25 +10,25 @@ const group17 = [
     {
         name: 'Christopher Aragon',
         role: 'Collaborative Mission Control Specialist',
-        image: '',
+        image: './src/assets/images/christopher-aragon.png',
         description: 'Current Senior Undergraduate.'
     },
     {
         name: "Bryan Cooke",
         role: 'LIDAR Navigation Specialist',
-        image: '',
+        image: './src/assets/images/bryan-cooke.png',
         description: 'Current Senior Undergraduate.'
     },
     {
         name: "Chirstopher Anastatis",
         role: 'LIDAR Navigation Specialist',
-        image: '',
+        image: './src/assets/images/christopher-anastasis.png',
         description: 'Current Senior Undergraduate.'
     },
     {
         name: "Charles Fernandez-Hamoui",
         role: 'LIDAR Navigation Specialist/',
-        image: '',
+        image: './src/assets/images/charles-fernandez-hamoui-pic.png',
         description: 'Current Senior Undergraduate.'
     }
 ]
@@ -51,10 +51,10 @@ function About() {
                 </div>
                 
                         
-                <ul role="list" className="flex flex-wrap gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+                <ul role="list" className="py-20 flex justify-center text-center">
                 {group17.map((member) => (
                     <li key={member.name} className="">
-                        <div className="items-center text-center p-4 gap-x-6" id='member-element'>
+                        <div className="items-center text-center p-8" id='member-element'>
                             <img className="h-48 w-48 rounded-xl mx-auto" src={member.image} alt="group member 17" />
                             <div>
                             <h3 className="text-base font-semibold leading-7 tracking-tight text-white">{member.name}</h3>
