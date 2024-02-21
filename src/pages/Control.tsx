f138a2cb9a3d750fbeb32dae90084bb4d8d5c21f
Francisco <paquito1214.2017@gmail.com>
2024-02-21 00:17:49 -0500

    added surveillance controls

--- src/pages/Control.tsx
+++ src/pages/Control.tsx
@@ -1,8 +1,36 @@
 import Navbar from '../components/Navbar'
+import ROSLIB from 'roslib'
 
 function Control() {
     return (
+    <>
         <Navbar />
+        {/* Controler */}
+        <div className='container flex mx-auto p-8'>
+            <div id='controls' className='grid grid-cols-3 gap-4 '>
+
+                <div className='grid-cols-subgrid col-span-3 mx-auto'>
+                    <button 
+                    className="bg-blue-400 hover:bg-blue-700 text-black text-2xl font-bold p-10 py-4 rounded"
+                    >↑</button>
+                </div>
+                
+                <button 
+                className="bg-blue-400 hover:bg-blue-700 text-black text-2xl font-bold p-10 py-4 rounded"
+                >←</button>
+                <button 
+                className="bg-blue-400 hover:bg-blue-700 text-black text-2xl font-bold p-10 py-4 rounded"
+                >↓</button>
+                <button 
+                className="bg-blue-400 hover:bg-blue-700 text-black text-2xl font-bold p-10 py-4 rounded"
+                >→</button>
+            </div>
+
+            <div>
+                something
+            </div>
+        </div>
+    </>
     )
 }
 
