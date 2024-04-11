029b225882170ab36f62b30cf7c82ca147ef26a3
Christopher Aragon <christopher.aragon2018@gmail.com>
2024-04-10 20:41:25 -0400

    Update Search.tsx

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -13,10 +13,34 @@ function Search() {
     }
 
     const handleConfirm = () => {
-        // Handle the confirmation here. For example, you can send the image to your server or to the robot.
-        console.log('Image confirmed')
-        setIsConfirmed(true) // Set the confirmation state to true when the image is confirmed
+        // Create a FormData object
+        let formData = new FormData();
+
+        // Append the selected image file to the FormData object
+        formData.append('image', selectedImage);
+
+        // Send a POST request to the server with the image file
+        fetch('http://10.12.42.22:3000/image', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+
+        setIsConfirmed(true); // Set the confirmation state to true when the image is confirmed
     }
+
+
+    const handleSearch = () => {
+        console.log('Start search')
+
+        fetch('http://10.12.42.22:3000/start_vision')
+            .then(response => response.text())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+    }
+
 
     return (
         <>
@@ -30,13 +54,20 @@ function Search() {
                     </div>
                 )}
 
-                <input type="file" accept="image/*" onChange={handleImageUpload} className="hidden" id="fileUpload" />
-                <button className="btn btn-secondary" onClick={() => document.getElementById('fileUpload')?.click()}>Upload Image</button>
-                {selectedImage && (
+                {!isConfirmed && (
+                    <>
+                        <input type="file" accept="image/*" onChange={handleImageUpload} className="hidden" id="fileUpload" />
+                        <button className="btn btn-secondary" onClick={() => document.getElementById('fileUpload')?.click()}>Upload Image</button>
+                    </>
+                )}
+                {selectedImage && !isConfirmed && (
                     <div className="mt-4">
                         <img src={selectedImage} alt="Selected" className="w-48 h-48 object-cover" />
                         <button className="btn btn-secondary mt-4" onClick={handleConfirm}>Confirm</button>
                     </div>
+                )}
+                {isConfirmed && (
+                    <button className="btn btn-secondary mt-4" onClick={handleSearch}>Start Search</button>
                 )}
             </div>
         </>
